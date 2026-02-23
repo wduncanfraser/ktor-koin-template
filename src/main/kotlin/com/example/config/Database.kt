42cd11f2b4f9b847f76d1a54a2ea01b6ef859568
W. Duncan Fraser <duncan@relianteng.io>
2026-02-22 23:55:51 -0600

    Added lettuce metrics and health check (#49)

--- src/main/kotlin/com/example/config/Database.kt
+++ src/main/kotlin/com/example/config/Database.kt
@@ -1,7 +1,12 @@
 package com.example.config
 
+import com.sksamuel.cohort.HealthCheck
+import com.sksamuel.cohort.HealthCheckResult
 import io.ktor.server.application.Application
 import io.ktor.server.config.property
+import io.micrometer.core.instrument.Gauge
+import io.micrometer.core.instrument.Tag
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import io.r2dbc.pool.ConnectionPool
 import io.r2dbc.pool.ConnectionPoolConfiguration
 import io.r2dbc.spi.ConnectionFactories
@@ -64,3 +69,56 @@ data class DatabaseConfig(
     val password: String,
     val poolSize: Int,
 )
+
+/**
+ * Function to register a set of R2DBC [ConnectionPool] metrics to a [PrometheusMeterRegistry]
+ */
+fun ConnectionPool.bindTo(registry: PrometheusMeterRegistry) {
+    metrics.ifPresent { metrics ->
+        val tags = listOf(Tag.of("name", this.metadata.name))
+
+        Gauge.builder("r2dbc.pool.connections.acquired", metrics) { it.acquiredSize().toDouble() }
+            .description("Connections currently acquired from the pool")
+            .tags(tags)
+            .register(registry)
+        Gauge.builder("r2dbc.pool.connections.allocated", metrics) { it.allocatedSize().toDouble() }
+            .description("Total connections currently allocated in the pool")
+            .tags(tags)
+            .register(registry)
+        Gauge.builder("r2dbc.pool.connections.idle", metrics) { it.idleSize().toDouble() }
+            .description("Connections currently idle in the pool")
+            .tags(tags)
+            .register(registry)
+        Gauge.builder("r2dbc.pool.connections.pending", metrics) { it.pendingAcquireSize().toDouble() }
+            .description("Pending connection acquisition requests")
+            .tags(tags)
+            .register(registry)
+        Gauge.builder("r2dbc.pool.connections.max.allocated", metrics) { it.maxAllocatedSize.toDouble() }
+            .description("Maximum connections allowed in the pool")
+            .tags(tags)
+            .register(registry)
+    }
+}
+
+/**
+ * Health check for the amount of allocated connections in an R2DBC [ConnectionPool]
+ *
+ * The check is considered healthy if the allocated connection count - idle and active - is >= [minAllocated].
+ */
+class ConnectionPoolMinAllocated(
+    private val pool: ConnectionPool,
+    private val minAllocated: Int,
+    override val name: String = "r2dbc_connections",
+) : HealthCheck {
+    override suspend fun check(): HealthCheckResult {
+        val metrics = pool.metrics.orElse(null)
+            ?: return HealthCheckResult.unhealthy("Pool metrics unavailable", null)
+        val allocated = metrics.allocatedSize()
+        val msg = "$allocated connections allocated to pool ${pool.metadata.name} [minAllocated: $minAllocated]"
+        return if (allocated >= minAllocated) {
+            HealthCheckResult.healthy(msg)
+        } else {
+            HealthCheckResult.unhealthy(msg, null)
+        }
+    }
+}
--- src/main/kotlin/com/example/config/Monitoring.kt
+++ src/main/kotlin/com/example/config/Monitoring.kt
@@ -1,9 +1,8 @@
 package com.example.config
 
 import com.sksamuel.cohort.Cohort
-import com.sksamuel.cohort.HealthCheck
 import com.sksamuel.cohort.HealthCheckRegistry
-import com.sksamuel.cohort.HealthCheckResult
+import com.sksamuel.cohort.lettuce.RedisHealthCheck
 import com.sksamuel.cohort.micrometer.CohortMetrics
 import com.sksamuel.cohort.threads.ThreadDeadlockHealthCheck
 import io.ktor.http.*
@@ -13,8 +12,7 @@ import io.ktor.server.plugins.callid.*
 import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.micrometer.core.instrument.Gauge
-import io.micrometer.core.instrument.Tag
+import io.lettuce.core.api.StatefulRedisConnection
 import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import io.r2dbc.pool.ConnectionPool
@@ -31,6 +29,7 @@ val monitoringModule = module {
 fun Application.configureMonitoring() {
     val prometheusRegistry by inject<PrometheusMeterRegistry>()
     val pool by inject<ConnectionPool>()
+    val redisConnection by inject<StatefulRedisConnection<String, String>>()
 
     install(CallId) {
         header(HttpHeaders.XRequestId)
@@ -45,6 +44,8 @@ fun Application.configureMonitoring() {
     val healthChecks = HealthCheckRegistry(Dispatchers.Default) {
         // Alert if we have no open R2DBC pool connections
         register(ConnectionPoolMinAllocated(pool, 1))
+        // Ensure redis is connecting succesfully
+        register(RedisHealthCheck(redisConnection))
         // Ensure threads aren't deadlocked
         register(ThreadDeadlockHealthCheck())
     }
@@ -65,59 +66,6 @@ fun Application.configureMonitoring() {
     routing {
         get("/metrics") {
             call.respond(prometheusRegistry.scrape())
-        }
-    }
-}
-
-/**
- * Function to register a set of R2DBC [ConnectionPool] metrics to a [PrometheusMeterRegistry]
- */
-fun ConnectionPool.bindTo(registry: PrometheusMeterRegistry) {
-    metrics.ifPresent { metrics ->
-        val tags = listOf(Tag.of("name", this.metadata.name))
-
-        Gauge.builder("r2dbc.pool.connections.acquired", metrics) { it.acquiredSize().toDouble() }
-            .description("Connections currently acquired from the pool")
-            .tags(tags)
-            .register(registry)
-        Gauge.builder("r2dbc.pool.connections.allocated", metrics) { it.allocatedSize().toDouble() }
-            .description("Total connections currently allocated in the pool")
-            .tags(tags)
-            .register(registry)
-        Gauge.builder("r2dbc.pool.connections.idle", metrics) { it.idleSize().toDouble() }
-            .description("Connections currently idle in the pool")
-            .tags(tags)
-            .register(registry)
-        Gauge.builder("r2dbc.pool.connections.pending", metrics) { it.pendingAcquireSize().toDouble() }
-            .description("Pending connection acquisition requests")
-            .tags(tags)
-            .register(registry)
-        Gauge.builder("r2dbc.pool.connections.max.allocated", metrics) { it.maxAllocatedSize.toDouble() }
-            .description("Maximum connections allowed in the pool")
-            .tags(tags)
-            .register(registry)
-    }
-}
-
-/**
- * Health check for the amount of allocated connections in an R2DBC [ConnectionPool]
- *
- * The check is considered healthy if the allocated connection count - idle and active - is >= [minAllocated].
- */
-class ConnectionPoolMinAllocated(
-    private val pool: ConnectionPool,
-    private val minAllocated: Int,
-    override val name: String = "r2dbc_connections",
-) : HealthCheck {
-    override suspend fun check(): HealthCheckResult {
-        val metrics = pool.metrics.orElse(null)
-            ?: return HealthCheckResult.unhealthy("Pool metrics unavailable", null)
-        val allocated = metrics.allocatedSize()
-        val msg = "$allocated connections allocated to pool ${pool.metadata.name} [minAllocated: $minAllocated]"
-        return if (allocated >= minAllocated) {
-            HealthCheckResult.healthy(msg)
-        } else {
-            HealthCheckResult.unhealthy(msg, null)
         }
     }
 }
